b7b6a9abfe5e1592732b5c786941ae8524eaf1ba
kiss2u <joe.lamplight@gmail.com>
2024-11-05 14:16:33 +0800

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-from gevent import pywsgi
-from geventwebsocket.handler import WebSocketHandler
-
-if __name__ == '__main__':
-    app = create_app()
-    server = pywsgi.WSGIServer(('0.0.0.0', 8080), app, handler_class=WebSocketHandler)
-    server.serve_forever()
